2be08959c9eef2f0ed98abe252c75bec2ac71c92
Juan Ignacio Celli <jcelli@giga.com>
2024-05-07 19:32:24 -0300

    Se modifica CORS

--- src/server.js
+++ src/server.js
@@ -26,15 +26,11 @@ import cors from 'cors'
 
 const mongoUrlDb = config.urlMongo
 const app = express()
-app.options('*', cors({
-    origin: '*',
+
+app.use(cors({
+    origin: 'http://localhost:5173',
     credentials: true
     }))
-
-// app.use(cors({
-//     origin: '*',
-//     credentials: true
-//     }))
 
 app.use(cookieParser())
 app.use(express.json())
